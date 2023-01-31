d29dabc3328444724a2180b8d0d8986c0c94a444
Nuekaze <17549670+nuekaze@users.noreply.github.com>
2023-01-31 11:50:46 +0000

    Update hazumi.py

--- hazumi.py
+++ hazumi.py
@@ -4,11 +4,16 @@ import transformers
 from transformers import utils, pipeline, set_seed
 import torch
 
-TOKEN = '' # Discord bot token.
-MY_NAME = 'Hazuki#0000' # Discord bot user ID.
-CHANNEL = 0000000000000000000 # Discord channel to read messages from.
+import threading
+import time
+import asyncio
+import re
 
-HISTORY_LENGTH = 100 # The amount of history the bot will remember.
+TOKEN = ''
+MY_NAME = 'Hazumi'
+MODEL_NAME = "facebook/opt-125m"
+CHANNEL = 0000000000000000000
+
 history = open("history.txt", 'r').readlines()
 history = list(map(str.strip, history))
 
@@ -17,30 +22,55 @@ intents.messages = True
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-MODEL_NAME = "facebook/opt-125m"
 generator = pipeline('text-generation',
                      model=MODEL_NAME,
                      do_sample=True,
+                     max_new_tokens=100,
                      torch_dtype=torch.float)
+
+channel = None
+
+async def send_message(message):
+  await channel.send(message)
+
+def remove_emotes(message):
+    words = message.split(' ')
+    new = []
+    for w in words:
+        if re.search("<:(.)*:([0-9])*>", w) or re.search("<a:(.)*:([0-9]*>)", w):
+            new.append(w.split(':')[1])
+        else:
+            new.append(w)
+    return ' '.join(new)
 
 def make_response():
     global history
-    input_text = "\n".join(history)
-    set_seed(32)
-    response = generator(input_text,
-                         max_length=len(input_text)+100/2.6,
-                         num_beams=1, 
-                         num_return_sequences=1
-                         )[0]['generated_text']
-    
-    # Just generate some future chat and pick whatever Hazumi would have said and return it.
-    response = response.split('\n')[len(history):len(history)+5]
-    #print(response)
-    for r in response:
-        if r.split(': ')[0] == MY_NAME:
-            return r.split(': ')[1]
+    global wait
+    while 1:
+        if wait:
+            input_text = "\n".join(history)
+            set_seed(32)
+            response = generator(input_text,
+                                 num_beams=1, 
+                                 num_return_sequences=1
+                                 )[0]['generated_text']
 
-    return None
+            try:
+                #print('\n'+'\n'.join(response.split('\n')[len(history):len(history)+6])+'\n')
+                response = response.split('\n')[len(history):len(history)+6]
+                for i in range(5):
+                    if response[i].split(': ')[0] == MY_NAME and "http" not in response[i].split(': ')[1]:
+                        asyncio.run_coroutine_threadsafe(send_message(response[i].split(': ')[1]), client.loop)
+                        if response[i+1].split(': ')[0] == MY_NAME and "http" not in response[i+1].split(': ')[1]:
+                            time.sleep(2)
+                            asyncio.run_coroutine_threadsafe(send_message(response[i+1].split(': ')[1]), client.loop)
+                        break
+            except IndexError:
+                pass
+
+            wait = 0
+
+        time.sleep(5)
             
 wait = 0
 
@@ -48,23 +78,18 @@ wait = 0
 async def on_message(ctx):
     global history
     global wait
-    if ctx.channel.id == CHANNEL:
-        history.append("%s: %s" % (ctx.author, ctx.content))
-        if len(history) > HISTORY_LENGTH:
-            history = history[HISTORY_LENGTH:]
-            
-        open("history.txt", 'a').write("%s: %s\n" % (ctx.author, ctx.content)) # Save history on the go.
-        #print("%s: %s" % (ctx.author, ctx.content))
-        
-        if wait == 0 and "%s" % ctx.author != MY_NAME: # Don't try respond to new messages if we are already working out a response.
+    global channel
+    if ctx.channel.id == CHANNEL and "http" not in ctx.content:
+        message = remove_emotes(ctx.content)
+        channel = ctx.channel
+        history.append("%s: %s" % (str(ctx.author).split('#')[0], message))
+        if len(history) > 100:
+            history = history[100:]
+        open("history.txt", 'a').write("%s: %s\n" % (str(ctx.author).split('#')[0], message))
+        print("%s: %s" % (str(ctx.author).split('#')[0], message))
+        if wait == 0 and "%s" % str(ctx.author).split('#')[0] != MY_NAME:
             wait = 1
-            # This will probably need to go into its own thread at some point. It hangs the Discord connection until a resonse is made.
-            response = make_response()
 
-            if response:
-                #print(response)
-                #print(ctx.channel)
-                await ctx.channel.send(response)
-        wait = 0
-
+responder_thread = threading.Thread(target=make_response)
+responder_thread.start()
 client.run(TOKEN)
