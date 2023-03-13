18dc3d6731612bb7607a29445615aba9d1f99168
Nuekaze <17549670+nuekaze@users.noreply.github.com>
2023-03-13 10:45:11 +0100

    Try to add emote support.

--- hazumi.py
+++ hazumi.py
@@ -8,27 +8,34 @@ import threading
 import time
 import asyncio
 import re
+import random
 
 TOKEN = ''
 MY_NAME = 'Hazumi'
 MODEL_NAME = "facebook/opt-125m"
 CHANNEL = 0000000000000000000
 
+print("Load history")
 history = open("history.txt", 'r').readlines()
 history = list(map(str.strip, history))
 
+print("Load discord")
 intents = discord.Intents.none()
 intents.messages = True
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+print("Load model")
 generator = pipeline('text-generation',
                      model=MODEL_NAME,
                      do_sample=True,
-                     max_new_tokens=100,
+                     max_new_tokens=200,
                      torch_dtype=torch.float)
 
 channel = None
+last_m = ""
+
+emotes = {}
 
 async def send_message(message):
   await channel.send(message)
@@ -39,11 +46,22 @@ def remove_emotes(message):
     for w in words:
         if re.search("<:(.)*:([0-9])*>", w) or re.search("<a:(.)*:([0-9]*>)", w):
             new.append(w.split(':')[1])
+            emotes[w.split(':')[1]] = w
         else:
             new.append(w)
     return ' '.join(new)
 
-next_force_response = 5
+def add_emotes(message):
+    words = message.split(' ')
+    new = []
+    for w in words:
+        try:
+            new.append(emotes[w])
+        except KeyError:
+            new.append(w)
+    return ' '.join(new)
+
+next_force_response = 3
 force_response = 0
 
 def make_response():
@@ -51,21 +69,23 @@ def make_response():
     global wait
     global next_force_response
     global force_response
+    global last_m
     while 1:
         if wait:
             input_text = "\n".join(history)
-            set_seed(32)
+            set_seed(random.randint(0, 1000))
             response = generator(input_text,
                                 num_beams=1, 
                                 num_return_sequences=1
                                 )[0]['generated_text']
 
             try:
-                response = response.split('\n')[len(history):len(history)+6]
+                response = response.split('\n')[len(history):]
+                #print(response)
                 for i in range(5):
-                    if response[i].split(': ')[0] == MY_NAME and "http" not in response[i].split(': ')[1]:
+                    if response[i].split(': ')[0] == MY_NAME and "http" not in response[i].split(': ')[1] and response[i].split(': ')[1] != last_m:
                         asyncio.run_coroutine_threadsafe(
-                                send_message(response[i].split(': ')[1]), client.loop)
+                                send_message(add_emotes(response[i].split(': ')[1])), client.loop)
                         
                         next_force_response = random.randint(3, 10)
                         force_response = 0
@@ -73,13 +93,14 @@ def make_response():
                         if response[i+1].split(': ')[0] == MY_NAME and "http" not in response[i+1].split(': ')[1]:
                             time.sleep(2)
                             asyncio.run_coroutine_threadsafe(
-                                    send_message(response[i+1].split(': ')[1]), client.loop)
+                                    send_message(add_emotes(response[i+1].split(': ')[1])), client.loop)
+                        last_m = response[i].split(': ')[1]
                         break
 
                 if force_response >= next_force_response:
-                    if "http" not in response[0].split(': ')[1] and "Hazuki" not in response[0].split(': ')[1]:
+                    if "http" not in response[1].split(': ')[1] and "Hazuki" not in response[1].split(': ')[1]:
                         asyncio.run_coroutine_threadsafe(
-                                send_message(response[0].split(': ')[1]), client.loop)
+                                send_message(add_emotes(response[1].split(': ')[1])), client.loop)
                     force_response = 0
                     next_force_response = random.randint(3, 10)
                     
@@ -91,7 +112,7 @@ def make_response():
 
             wait = 0
 
-        time.sleep(3)
+        time.sleep(1)
             
 wait = 0
 
@@ -115,6 +136,8 @@ async def on_message(ctx):
         if wait == 0 and "%s" % author != MY_NAME:
             wait = 1
 
+print("Start model thread")
 responder_thread = threading.Thread(target=make_response)
 responder_thread.start()
+print("Start discord")
 client.run(TOKEN)
